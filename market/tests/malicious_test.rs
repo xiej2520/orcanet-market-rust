8a8496569d53a65fb4971ed296b408ee4172d9b8
tamandrew <andrew.c.tam@stonybrook.edu>
2024-03-31 19:49:22 -0400

    Update tests

---
+++ market/tests/malicious_test.rs
@@ -0,0 +1,78 @@
+use lib_proto::User;
+use orcanet_market_ferrous::{dht, get_current_time, FileRequest};
+use orcanet_market_ferrous::dht::DhtClient;
+use rand::{distributions::Alphanumeric, Rng};
+
+
+fn random_string(len: usize) -> String {
+    let s: String = rand::thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(len)
+        .map(char::from)
+        .collect();
+
+    s
+}
+
+async fn create_test_client() -> dht::DhtClient {
+
+    let str = "/ip4/130.245.173.204/tcp/6882/p2p/QmXQ2cfTwrNVAqepupQQYdsyrHsscR2TWkghWxYUjNLmKU".to_string();
+    let bootstrap_peers = vec![str.parse().unwrap()];
+
+    let listen_on = None;
+
+    let (dht_client, _) = match DhtClient::spawn_client(&bootstrap_peers, listen_on).await
+    {
+        Ok(o) => o,
+        Err(err) => {
+            panic!("Failed to spawn client: {}", err);
+        },
+    };
+
+    dht_client
+}
+
+
+#[tokio::test]
+async fn test_malicious_delete() {
+    let dht_client = create_test_client().await;
+
+    //valid request
+    let file_hash = random_string(32);
+    let user1 = User {
+        id: random_string(10),
+        name: random_string(10),
+        ip: random_string(10),
+        port: 1,
+        price: 1
+    };
+    let fr1 = FileRequest {
+        user: user1.clone(),
+        file_hash: file_hash.clone(),
+        expiration: get_current_time() + 100
+    };
+
+    let requests = vec![fr1];
+    let _res = dht_client.set_requests(&file_hash, requests).await;
+
+    // try to "delete" the request by setting expiration to the past
+    let fr2 = FileRequest {
+        user: user1.clone(),
+        file_hash: file_hash.clone(),
+        expiration: get_current_time() - 1000
+    };
+
+    let requests = vec![fr2];
+    let _res = dht_client.set_requests(&file_hash, requests).await;
+
+    let end_holders = dht_client.get_requests(&file_hash).await.unwrap();
+    match end_holders {
+        Some(holders) => {
+            assert_eq!(holders.len(), 1);
+        },
+        None => {
+            panic!("Failed to get holders");
+        }
+    }
+
+}
